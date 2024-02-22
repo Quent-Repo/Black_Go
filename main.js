a3e62fbac3557ce16c5b81e94ead2bdf2bbbfea9
owner <grrkekisbest@gmail.com>
2024-02-22 13:32:40 -0500

    Name change for better understanding

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 document.getElementsByTagName("body")[0].style.cssText = style="background-color:black"
 
 // Code block
-let f = document.getElementsByClassName("code leading")
-for (let i=0; i<f.length;i++){
-    f[i].style.cssText = style="background-color:black"
+let Number_Of_Parts = document.getElementsByClassName("code leading")
+for (let i=0; i<Number_Of_Parts.length;i++){
+    Number_Of_Parts[i].style.cssText = style="background-color:black"
 }
+
