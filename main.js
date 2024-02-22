345778f641b48fa05d1c72d27a05ad5f675db561
owner <grrkekisbest@gmail.com>
2024-02-22 13:30:18 -0500

    Added in the code block as well

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+//Body background
 document.getElementsByTagName("body")[0].style.cssText = style="background-color:black"
+
+// Code block
+let f = document.getElementsByClassName("code leading")
+for (let i=0; i<f.length;i++){
+    f[i]..style.cssText = style="background-color:black"
+}
