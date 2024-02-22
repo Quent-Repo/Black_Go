51003d2ada091f266f6b446629e3ad86bc790816
owner <grrkekisbest@gmail.com>
2024-02-22 14:01:40 -0500

    Fixed bug where part of the code block was set to white.
    Added in ```var Color``` to allow for easy color changing

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
+var Color = "black"
+
 //Body background
-document.getElementsByTagName("body")[0].style.cssText = style="background-color:black"
+document.getElementsByTagName("body")[0].style.cssText = style=`background-color:${Color}`
 
 // Code block
 let Number_Of_Parts = document.getElementsByClassName("code leading")
 for (let i=0; i<Number_Of_Parts.length;i++){
-    Number_Of_Parts[i].style.cssText = style="background-color:black"
+    Number_Of_Parts[i].style.cssText = style=`background-color:${Color}`
 }
 
 // Code empty
 let code_empty = document.getElementsByClassName("code empty")
 for (let i=0; i<code_empty.length;i++){
-    code_empty[i].style.cssText = style="background-color:black"
+    code_empty[i].style.cssText = style=`background-color:${Color}`
+}
+
+//code
+let Chroma = document.getElementsByClassName("code")
+for (let i=0; i<Chroma.length;i++){
+    Chroma[i].style.cssText = style=`background-color:${Color}`
 }
