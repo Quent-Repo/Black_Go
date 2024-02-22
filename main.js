0d1bb1d2f359b56d5f1ba45ee4d07449cdc99894
owner <grrkekisbest@gmail.com>
2024-02-22 13:35:22 -0500

    Added in code for empty code block also

--- main.js
+++ main.js
@@ -7,3 +7,8 @@ for (let i=0; i<Number_Of_Parts.length;i++){
     Number_Of_Parts[i].style.cssText = style="background-color:black"
 }
 
+// Code empty
+let code_empty = document.getElementsByClassName("code empty")
+for (let i=0; i<code_empty.length;i++){
+    code_empty[i].style.cssText = style="background-color:black"
+}
